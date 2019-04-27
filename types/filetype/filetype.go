6afa0c32a88493dcff5523c854f2818a262ef24e
zzzhr <zzzhr@hotmail.com>
2019-04-27 23:22:58 +0800

    add filetype

---
+++ types/filetype/filetype.go
@@ -0,0 +1,18 @@
+package filetype
+
+// github.com/zzzhr1990/go-common-entity/types/filetype
+
+const (
+	// UnknownType unknown
+	UnknownType int32 = 0
+	// DirectoryType for UNIX directory
+	DirectoryType int32 = 10
+	// ImageType for normal images
+	ImageType int32 = 20
+	// VideoType for video
+	VideoType int32 = 30
+	// OfficeType word, excel,powerpoint...
+	OfficeType int32 = 40
+	// AudioType for audio files
+	AudioType int32 = 50
+)
