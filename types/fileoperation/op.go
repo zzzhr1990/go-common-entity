cd082453d262a9772e5c439bfc14ec6bbdd82c1c
zzzhr <zzzhr@hotmail.com>
2019-04-29 01:14:28 +0800

    add fileoperation

---
+++ types/fileoperation/op.go
@@ -0,0 +1,16 @@
+package fileoperation
+
+// github.com/zzzhr1990/go-common-entity/types/fileoperation
+
+const (
+	// CreateFileOperation when file created
+	CreateFileOperation = 1
+	// DeleteFileOperation when file deleted
+	DeleteFileOperation = 2
+	// MoveFileOperation when file move
+	MoveFileOperation = 3
+	// CopyFileOperation when file copy
+	CopyFileOperation = 4
+	// RenameOperation when file rename
+	RenameOperation = 5
+)
