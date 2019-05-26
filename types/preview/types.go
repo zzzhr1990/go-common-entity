ee8fec59e0dcb83df0148ee55f6f18cf906a5f98
zzzhr <zzzhr@hotmail.com>
2019-05-27 00:38:15 +0800

    add preview

---
+++ types/preview/types.go
@@ -0,0 +1,12 @@
+package preview
+
+// github.com/zzzhr1990/go-common-entity/types/preview
+
+const (
+	// Unknown unknown
+	Unknown int32 = 0
+	// HLS HLS play
+	HLS int32 = 3010
+	// MP3 HLS play
+	MP3 int32 = 3020
+)
