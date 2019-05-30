e9f7f10568cb2dd4bca4c83fbc501152c288a769
zzzhr <zzzhr@hotmail.com>
2019-05-31 05:34:05 +0800

    add new ar

--- tasks/preview/preview.go
+++ tasks/preview/preview.go
@@ -8,5 +8,9 @@ const (
 	VideoPreviewGenerate int32 = 7010
 	// AudioPreviewGenerate generate audio
 	AudioPreviewGenerate int32 = 7020
+	// ImagePreviewGenerate generate audio
+	ImagePreviewGenerate int32 = 7030
+	// ArchivePreviewGenerate generate audio
+	ArchivePreviewGenerate int32 = 7040
 	//////////////////////////
 )
