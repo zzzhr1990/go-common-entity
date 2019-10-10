2b1a5a575e1b54945b606ced1daffc5bb6bcbfad
zzzhr <zzzhr@hotmail.com>
2019-10-11 02:31:21 +0800

    remove prefix

--- confs/mysql_conf.go
+++ confs/mysql_conf.go
@@ -2,10 +2,10 @@ package confs
 
 // MySQLConf config for common
 type MySQLConf struct {
-	SlaveDatabaseConnectionString  []string `yaml:"slave-database-connection-string"`
-	MasterDatabaseConnectionString []string `yaml:"master-database-connection-string"`
-	DatabaseDialect                string   `yaml:"database-dialect"`
-	ReadWriteSeparate              bool     `yaml:"read-write-separate"`
-	AutoInitialize                 string   `yaml:"auto-initialize"`
-	TableNamePrefix                string   `yaml:"table-name-prefix"`
+	SlaveConnectionString  []string `yaml:"slave-connection-string"`
+	MasterConnectionString []string `yaml:"master-connection-string"`
+	Dialect                string   `yaml:"dialect"`
+	ReadWriteSeparate      bool     `yaml:"read-write-separate"`
+	AutoInitialize         string   `yaml:"auto-initialize"`
+	TableNamePrefix        string   `yaml:"table-name-prefix"`
 }
