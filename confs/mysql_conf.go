f3f885d0e0d28006ac2c1b59cee7f40dd58fb631
zzzhr-pc <zzzhr@hotmail.com>
2020-04-02 01:48:10 +0800

    bump version

--- confs/mysql_conf.go
+++ confs/mysql_conf.go
@@ -6,6 +6,16 @@ type MySQLConf struct {
 	MasterConnectionString []string `yaml:"master-connection-string"`
 	Dialect                string   `yaml:"dialect"`
 	ReadWriteSeparate      bool     `yaml:"read-write-separate"`
-	AutoInitialize         string   `yaml:"auto-initialize"`
+	AutoInitialize         bool     `yaml:"auto-initialize"`
 	TableNamePrefix        string   `yaml:"table-name-prefix"`
+}
+
+// MySQLMultiDbConf config for common just for mysql
+type MySQLMultiDbConf struct {
+	NamePrefix []string `yaml:"name-prefix"`
+	// MasterConnectionString []string `yaml:"master-connection-string"`
+	Dbs             map[string]string `yaml:"dbs"`
+	Dialect         string            `yaml:"dialect"`
+	AutoInitialize  bool              `yaml:"auto-initialize"`
+	TableNamePrefix string            `yaml:"table-name-prefix"`
 }
