2958aa009dc1ca9a7accffce6788e28f0928ffbb
zzzhr-pc <zzzhr@hotmail.com>
2020-04-20 16:14:54 +0800

    fix

--- types/preview/types.go
+++ types/preview/types.go
@@ -5,17 +5,14 @@ package preview
 const (
 	// Unknown unknown
 	Unknown int32 = 0
-	// HLS HLS play
+	// HLS Apple live stream play
 	HLS int32 = 3010
-	// MP3 HLS play
+	// MP3 file play
 	MP3 int32 = 3020
-
 	// Image img
 	Image int32 = 3030
-
-	// Document prw
+	// Document Use Microsoft office 365 preview
 	Document int32 = 3040
-
-	// Archive prw
+	// Archive arc
 	Archive int32 = 3050
 )
