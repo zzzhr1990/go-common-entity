57bae027ceaf47bd1e06bc89f2998742c72c5dbc
zzzhr <zzzhr@hotmail.com>
2020-04-21 15:20:43 +0800

    chl

--- offline/offline_user_task_data.go
+++ offline/offline_user_task_data.go
@@ -1,11 +1,22 @@
 package offline
 
 // TaskData detail for store
-// github.com/zzzhr1990/go-common-entity
+// github.com/zzzhr1990/go-common-entity/offline
 type TaskData struct {
 	URL      string `json:"url,omitempty"`
 	Identity string `json:"identity,omitempty"`
 	File     string `json:"file,omitempty"`
 	User     int64  `json:"user"`
 	Path     string `json:"path,omitempty"`
+}
+
+// ServerData data for server
+type ServerData struct {
+	URL           string `json:"url,omitempty"`
+	ShareIdentity string `json:"shareIdentity,omitempty"`
+	OwnerIdentity string `json:"ownerIdentity,omitempty"`
+	CreateTime    int64  `json:"createTime,omitempty"`
+	Path          string `json:"path,omitempty"`
+	Charset       string `json:"charset,omitempty"`
+	Password      string `json:"password,omitempty"`
 }
