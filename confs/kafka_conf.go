135c1b683d3ea67457cf6192b0b1531ee4ae8474
zzzhr <zzzhr@hotmail.com>
2020-08-14 04:45:23 +0800

    support KafkaConsumerConfig

---
+++ confs/kafka_conf.go
@@ -0,0 +1,21 @@
+package confs
+
+// KafkaConsumerConfig support qc
+type KafkaConsumerConfig struct {
+	Username  string   `yaml:"username"`
+	Password  string   `yaml:"password"`
+	Mechanism string   `yaml:"mechanism"`
+	GroupID   string   `yaml:"group-id"`
+	Topic     string   `yaml:"topic"`
+	Brokers   []string `yaml:"brokers"`
+}
+
+// KafkaProducerConfig support qc
+type KafkaProducerConfig struct {
+	Username  string   `yaml:"username"`
+	Password  string   `yaml:"password"`
+	Mechanism string   `yaml:"mechanism"`
+	Topic     string   `yaml:"topic"`
+	Async     bool     `yaml:"async"`
+	Brokers   []string `yaml:"brokers"`
+}
